58ff93664ad97af5c09f694b8f2b7d95148790e4
Dhron Patel <dhronp110@gmail.com>
2025-07-22 01:22:12 -0500

    no backend just front end

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,4 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
-import NotFound from "@/pages/not-found";
+// client/src/App.tsx
 
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Router />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
+// 1. Import Router from wouter
+import { Switch, Route, Router as WouterRouter } from "wouter"
